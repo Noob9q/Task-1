873d3e134ed4376cfa19f8da801f337efba290cf
Noob9q <lookseek000@gmail.com>
2024-12-25 09:08:10 +0500

    Task
    Task 1

---
+++ Person.java
@@ -0,0 +1,39 @@
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+    private boolean gender;
+
+    public Person(String name, String surname, int age, boolean gender) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.gender = gender;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public boolean isGender() {
+        return gender;
+    }
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+
+    public String toString() {
+        return "My name is "+name+ " "+ surname+
+                "\nyears old "+age+" "+(gender? "male":"female");
+    }
+}
